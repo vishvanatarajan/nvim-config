d2436c9f6aa701ee1a563102bed0c21fec909b1e
Vishva Natarajan <vishvanatarajan@gmail.com>
2026-02-04 14:54:12 +0530

    Disable hererocks and luarocks in Lazy config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -50,6 +50,10 @@ require('lazy').setup({
     -- import your plugins
     { import = 'plugins' },
   },
+  rocks = {
+    hererocks = false,
+    enabled = false,
+  },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { 'habamax' } },
